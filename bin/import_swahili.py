1bcfd17bde39de12ea97e877b1963f1f5fae6abc
Massiel Islas Malanco <mislas@simplenexus.com>
2019-12-10 09:19:14 -0700

    files to train in swahili

---
+++ bin/import_swahili.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function
+
+# Make sure we can import stuff from util/
+# This script needs to be run from the root of the DeepSpeech repository
+import sys
+import os
+import pandas
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+def _download_and_preprocess_data(data_dir):
+  overview_of_recordings = pandas.read_csv("data/swahili/overview-of-recordings.csv")
+  overview_of_recordings.set_index("file_name", inplace=True)
+  print("OVERVIEW HEAD")
+  print(overview_of_recordings.head())
+
+  swahili_training = pandas.DataFrame()
+
+  for filename in os.listdir(data_dir + "/recordings/train/"):
+    if filename.endswith(".wav"):
+      # print(filename)
+      recording_row = overview_of_recordings.loc[filename]
+      recording_row[['_unit_id']] = os.path.getsize(data_dir + "/recordings/train/" + filename)
+      swahili_training = swahili_training.append(recording_row[['_unit_id', 'sw_transcription']])
+  swahili_training.reset_index(inplace=True)
+  print("SWAHILI HEAD")
+  print(swahili_training.head())
+  swahili_training = swahili_training.rename(columns={"index": "wav_filename", "_unit_id": "wav_filesize", "sw_transcription": "transcript"}, errors="raise")
+  swahili_training['wav_filename'] = swahili_training['wav_filename'].apply(lambda x: "recordings/train/" + x)
+  swahili_training.to_csv(os.path.join(data_dir, "swahili_training.csv"), index=False)
+
+if __name__ == "__main__":
+    _download_and_preprocess_data(sys.argv[1])
+
+
+
+
+
+
+
---
+++ bin/run-swahili.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -xe
+if [ ! -f DeepSpeech.py ]; then
+    echo "Please make sure you run this from DeepSpeech's top level directory."
+    exit 1
+fi;
+
+if [ ! -f "data/swahili/swahili_training.csv" ]; then
+    echo "Downloading and preprocessing swahili example data, saving in ./data/swahili."
+    python -u bin/import_swahili.py ./data/swahili
+fi;
+
+if [ -d "${COMPUTE_KEEP_DIR}" ]; then
+    checkpoint_dir=$COMPUTE_KEEP_DIR
+else
+    checkpoint_dir=$(python -c 'from xdg import BaseDirectory as xdg; print(xdg.save_data_path("deepspeech/swahili"))')
+fi
+
+# Force only one visible device because we have a single-sample dataset
+# and when trying to run on multiple devices (like GPUs), this will break
+export CUDA_VISIBLE_DEVICES=0
+
+python -u DeepSpeech.py --noshow_progressbar \
+  --train_files data/swahili/swahili_training.csv \
+  --test_files data/swahili/swahili_training.csv \
+  --train_batch_size 1 \
+  --test_batch_size 1 \
+  --n_hidden 100 \
+  --epochs 200 \
+  --checkpoint_dir "$checkpoint_dir" \
+  "$@"
