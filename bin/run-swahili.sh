b1369fd3c8a70a180e371774d47b20dab9b7ddb4
Massiel Islas Malanco <mislas@simplenexus.com>
2019-12-11 16:22:27 -0700

    enable gpu usability

--- bin/run-swahili.sh
+++ bin/run-swahili.sh
@@ -16,9 +16,9 @@ else
     checkpoint_dir=$(python -c 'from xdg import BaseDirectory as xdg; print(xdg.save_data_path("deepspeech/swahili"))')
 fi
 
-# Force only one visible device because we have a single-sample dataset
-# and when trying to run on multiple devices (like GPUs), this will break
-export CUDA_VISIBLE_DEVICES=0
+# # Force only one visible device because we have a single-sample dataset
+# # and when trying to run on multiple devices (like GPUs), this will break
+# export CUDA_VISIBLE_DEVICES=0
 
 python -u DeepSpeech.py --noshow_progressbar \
   --train_files data/swahili/swahili_training.csv \
